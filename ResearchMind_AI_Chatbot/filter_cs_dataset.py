3b5569792291d480853a92759db9025a149f76e1
shriyansh-69 <shriyansh457tiwari@gmail.com>
2026-03-05 15:20:52 +0530

    Extracting Related From The Dataset And  Setting Limit For Number of Document's

--- ResearchMind_AI_Chatbot/filter_cs_dataset.py
+++ ResearchMind_AI_Chatbot/filter_cs_dataset.py
@@ -4,3 +4,13 @@ import os
 # Input And Output files
 input_file = "arxiv-metadata-oai-snapshot.json"
 output_file = "data/arxiv_cs.json"
+
+# Valid So It Doesn't Crash
+os.makedirs("data", exist_ok=True)
+
+
+# Extracting Related From The Dataset And  Setting Limit For Number of Document's
+cs_paper = []
+limit = 60000
+
+
