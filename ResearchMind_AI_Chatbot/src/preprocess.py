9dd0896e71fa4eee1f88af122cadf438abc041e9
shriyansh-69 <shriyansh457tiwari@gmail.com>
2026-03-05 15:52:34 +0530

    Importing All The Esstinal Step's

--- ResearchMind_AI_Chatbot/src/preprocess.py
+++ ResearchMind_AI_Chatbot/src/preprocess.py
@@ -4,3 +4,9 @@ import nltk
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
 
+
+nltk.download('stopwords')
+nltk.download('wordnet')
+
+lemmatizer = WordNetLemmatizer()
+stopwords = set(stopwords.words("english"))
