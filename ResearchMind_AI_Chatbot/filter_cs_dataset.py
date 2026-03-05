4e30123beb63f1454da4dd216b362fc26443aee2
shriyansh-69 <shriyansh457tiwari@gmail.com>
2026-03-05 15:32:01 +0530

    This Function For Extracting The Limited Number Of Document for Our Dataset

--- ResearchMind_AI_Chatbot/filter_cs_dataset.py
+++ ResearchMind_AI_Chatbot/filter_cs_dataset.py
@@ -10,7 +10,39 @@ os.makedirs("data", exist_ok=True)
 
 
 # Extracting Related From The Dataset And  Setting Limit For Number of Document's
-cs_paper = []
+cs_papers = []
 limit = 60000
+
+print("Filtering Computer Science papers...\n")
+
+with open(input_file,"r",encoding="utf-8") as f:
+    for line in f:
+        try:
+            paper = json.loads(line)
+
+            categories  = paper.get("categories","")
+
+            if "cs." in categories:
+                cs_papers.append({
+                    "id" : paper.get("id"),
+                    "title" : paper.get("title"),
+                    "abstract" : paper.get("abstract"),
+                    "categories" : categories,
+                    "update_date" : paper.get("update_date"),
+                })
+
+
+            # It IS For Stopping When The Data Hit's the Limit 
+            if len(cs_papers) >= limit:
+                break
+
+        except Exception:
+            continue  # SKIP Bad Lines Safely 
+
+print(f"Total CS papers extracted: {len(cs_papers)}")
+
+
+
+        
 
 
