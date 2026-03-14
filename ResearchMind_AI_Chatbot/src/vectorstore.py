04babcee3c823e3426d8a6a382ee8f82e88a9af6
shriyansh-69 <shriyansh457tiwari@gmail.com>
2026-03-14 15:43:44 +0530

    Importing The Esstinal Library

---
+++ ResearchMind_AI_Chatbot/src/vectorstore.py
@@ -0,0 +1,8 @@
+## This File Is For Creating The Embedding For Word Or Sentence's That Are Stored In Vector DB
+
+import json
+from langchain_community.vectorstores import FAISS
+from langchain_community.embeddings import HuggingFaceEmbeddings
+
+
+
