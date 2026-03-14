49c7091856a5c0a424741a2a983b80b5d5fb9827
shriyansh-69 <shriyansh457tiwari@gmail.com>
2026-03-14 15:53:19 +0530

    Loading The Dataset And Creating Embedding For It

--- ResearchMind_AI_Chatbot/src/vectorstore.py
+++ ResearchMind_AI_Chatbot/src/vectorstore.py
@@ -1,8 +1,17 @@
 ## This File Is For Creating The Embedding For Word Or Sentence's That Are Stored In Vector DB
 
+
+# Importing The Esstinal Library
 import json
 from langchain_community.vectorstores import FAISS
 from langchain_community.embeddings import HuggingFaceEmbeddings
 
 
+# Loading The Cleaned Dataset 
+with open('data/arxiv_cs_clean.json',"r",encoding="utf-8") as f:
+    papers = json.load(f)
 
+# Extracting The Text 
+texts = [paper["clean_abstract"] for paper in papers]
+
+print(f"Loaded {len(texts)} Document's")
