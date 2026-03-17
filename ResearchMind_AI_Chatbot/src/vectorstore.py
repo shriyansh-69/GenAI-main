9c72646a376dcf5ab12fb12d34b66daa3a4f3d4b
shriyansh-69 <shriyansh457tiwari@gmail.com>
2026-03-17 23:08:51 +0530

    "define the path"

--- ResearchMind_AI_Chatbot/src/vectorstore.py
+++ ResearchMind_AI_Chatbot/src/vectorstore.py
@@ -5,19 +5,28 @@
 import json
 import os
 from langchain_community.vectorstores import FAISS
-from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_huggingface import HuggingFaceEmbeddings
 
 
 # Using The Absolute Path 
 base_dir = os.path.dirname(os.path.dirname(__file__))
-data_path = os.path.join(base_dir,"data","arxiv_cs_clean.json")
+data_path = os.path.join(base_dir,"data","arxiv_cs.json")
 
 # Loading The Cleaned Dataset 
 with open(data_path,"r",encoding="utf-8") as f:
     papers = json.load(f)
 
+print(len(papers))
+
+
 # Extracting The Text 
-texts = [papers["clean_abstract"] for paper in papers if paper.get("clean_abstract")]
+texts = []
+
+for paper in papers:
+    text = paper.get("abstract")
+
+    if text and text.strip():
+        texts.append(text)
 
 print(f"Loaded {len(texts)} Document's")
 
@@ -25,6 +34,7 @@ print(f"Loaded {len(texts)} Document's")
 embeddings = HuggingFaceEmbeddings(
     model_name = "sentence-transformers/all-MiniLM-L6-v2"
 )
+
 
 # Generating text embeddings And Stored Them In FAISS
 vectorstore = FAISS.from_texts(texts,embeddings)
@@ -38,3 +48,5 @@ os.makedirs(vector_path,exist_ok=True)
 #Save FAISS index locally
 vectorstore.save_local(vector_path)
 
+print("FAISS vector store created successfully!")
+print(f"Saved at: {vector_path}")
