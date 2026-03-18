e05ab0cd132a019e9d28e514bff05a96f311bd15
shriyansh-69 <shriyansh457tiwari@gmail.com>
2026-03-18 21:30:55 +0530

    Define the path and load the env variable

--- ResearchMind_AI_Chatbot/src/rag_pipeline.py
+++ ResearchMind_AI_Chatbot/src/rag_pipeline.py
@@ -10,3 +10,18 @@ from langchain_community.vectorstores import FAISS
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_core.prompts import PromptTemplate
 from langchain.chains.retrieval_qa.base import RetrievalQA
+
+# -----------------------------------------
+# Load env variables
+# ----------------------------------------- 
+
+load_dotenv()
+
+# -----------------------------------------
+# Base Path
+# ----------------------------------------- 
+
+base_dir = os.path.dirname(os.path.dirname(__file__))
+vector_path = os.path.join(base_dir,"vector_store")
+
+
