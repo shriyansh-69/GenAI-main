0cd96f7eaf2f3dbae776a339d60a1d62c31ec7ca
shriyansh-69 <shriyansh457tiwari@gmail.com>
2026-03-21 21:54:21 +0530

    polish the final layout

--- ResearchMind_AI_Chatbot/app.py
+++ ResearchMind_AI_Chatbot/app.py
@@ -3,31 +3,40 @@
 # -----------------------------------------
 
 import streamlit as st
-from src.rag_pipeline import ask_question
-
+from src.rag_pipeline import ask_question,retriever
 
 # -----------------------------------------
 # Style
 # -----------------------------------------
 st.set_page_config(page_title="ResearchMind AI")
 
-st.title("🧠 Research AI Chatbot")
+st.title("🧠 Computer Science Research AI Chatbot")
+st.caption("Ask questions about Computer Science research papers")
 
+# -----------------------------------------
 # Store chat session history
-if "message" not in st.session_state:
+# -----------------------------------------
+if "messages" not in st.session_state:
     st.session_state.messages = []
 
+# -----------------------------------------
 # Show Previous messages
+# -----------------------------------------
 for msg in st.session_state.messages:
-    st.chat_message(msg["role"]).write(msg["content"])
+    avatar = "🧠" if msg["role"] == "assistant" else "👱"
+    with st.chat_message(msg["role"], avatar=avatar):
+        st.write(msg["content"])
 
-# Input Box
+
+# -----------------------------------------
+# Input box
+# -----------------------------------------
 query = st.chat_input("Ask your question...")
 
 if query:
     # show user message
-    st.session_state.messages.append({"role":"user","content":"query"})
-    st.chat_message("user").write(query)
+    st.session_state.messages.append({"role":"user","content": query})
+    st.chat_message("user",avatar="👱").write(query)
 
     # generate response 
     with st.spinner("Thinking...."):
@@ -35,5 +44,19 @@ if query:
 
     # show bot response 
     st.session_state.messages.append({"role":"assitant","content":answer})
-    st.chat_message("assistant").write(answer)
+    st.chat_message("assitant",avatar="🧠").write(answer)
+
+    # ---------- SHOW SOURCES ----------
+    docs = retriever.get_relevant_documents(query)
+
+    with st.expander("View Source"):
+        for i,doc in enumerate(docs):
+            st.markdown(f"**Source {i+1}:**")
+            st.write(doc.page_content[:300])
+            st.markdown("---")
+
+    # Save response
+    st.session_state.messages.append({"role":"assitant","content": answer})
+
+
 
