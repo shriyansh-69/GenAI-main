3455478f26a2fc32a6440c9cb3e9b7f7aeb035e6
shriyansh-69 <shriyansh457tiwari@gmail.com>
2026-04-06 11:05:54 +0530

    This file  is for processing image

---
+++ MultiModal_AI_Chatbot/src/image_handler.py
@@ -0,0 +1,22 @@
+# This File Is For Image Handling Logic Of Chatbot
+
+from google import genai
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+client = genai.Client(api_key=os.getenv("GOOGLE_API_KEY"))
+
+def process_image(user_input,image):
+
+    if not user_input:
+        user_input = "Describe this image"
+
+    response = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents=[user_input,   image]
+    )
+
+
+    return response.text
